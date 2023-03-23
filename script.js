474170a0daa9d95b11b0b1b17ec2ef9f419826f1
Tanvir chowdhury <chowdhurytanvir268@gmail.com>
2023-03-23 19:17:34 +0600

    Update file

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new typed(".multiple-text", {
+    strings: ["Frontend Developer" , "Youtuber" , "Bloger"], 
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
